6937aa075378917513feddc9a0bd1e24f2494f8f
Hary Prihadi <phdhary@gmail.com>
2023-09-02 10:40:31 +0700

    feat(script): add operate function that choose based on operator

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const add = (a, b) => a + b,
 let operator = null,
   firstNumber = null,
   secondNumber = null;
+
+const operate = (operator, a, b) => {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
+  }
+};
